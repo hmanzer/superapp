846d0472b0f494b630433f588e4e0e8a85d0a947
Humayun Manzer <hmanzer@protonmail.com>
2022-06-14 12:57:01 +0800

    fix environment variables

--- customers_api/db.py
+++ customers_api/db.py
@@ -1,16 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
-dotenv_path = '../.env'
-load_dotenv(dotenv_path)
+# from dotenv import load_dotenv, find_dotenv
+# from pathlib import Path
+
+
+# load_dotenv()
+# env_path = os.path.abspath('..') + '.env'
+# print(env_path)
+#load_dotenv(find_dotenv())
+
 
 db = SQLAlchemy()
 
 class DbConfig:
     def __init__(self):
-        self.DB_USERNAME = environ.get('DB_USERNAME')
-        self.DB_PASSWORD = environ.get('DB_PASSWORD')
+        self.DB_USERNAME = environ.get('MYSQL_USER')
+        self.DB_PASSWORD = environ.get('MYSQL_PASSWORD')
         self.DB_HOST = environ.get('DB_HOST')
         self.DB_PORT = int(environ.get('DB_PORT'))
         self.DB_NAME = environ.get('DB_NAME')
